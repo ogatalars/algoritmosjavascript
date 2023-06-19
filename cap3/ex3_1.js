51c68c28ff5e7bc44dfdaf69c29b3fa2cbbffdcb
ogatalars <ogataogara@gmail.com>
2023-06-19 15:09:17 -0300

    Add: new example using prompt sync

---
+++ cap3/ex3_1.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")(); // adiciona o pacote 'prompt-sync' ao código (entrada de dados)
+const num1 = Number(prompt("Digite o primeiro número: ")); // entrada de dados
+const num2 = Number(prompt("Digite o segundo número: ")); // entrada de dados
+const soma = num1 + num2;
+console.log("A soma dos números é: ", soma); // saída de dados
