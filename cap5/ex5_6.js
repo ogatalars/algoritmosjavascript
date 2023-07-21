ed30f647598841ae5fb79453e489b077d2e61799
ogatalars <ogataogara@gmail.com>
2023-07-20 22:09:16 -0300

    Fix a problem on the example

--- cap5/ex5_6.js
+++ cap5/ex5_6.js
@@ -3,4 +3,16 @@ const resp = document.querySelector("pre");
 
 frm.addEventListener("submit", function (e) {
   e.preventDefault();
+  const num = Number(frm.inNumero.value);
+  let numDivisores = 0;
+  for (let i = 1; i <= num; i++) {
+    if (num % i == 0) {
+      numDivisores++;
+    }
+  }
+  if (numDivisores == 2) {
+    resp.textContent = `${num} é primo`;
+  } else {
+    resp.textContent = `${num} não é primo`;
+  }
 });
