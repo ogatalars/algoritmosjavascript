56d06e491ea68a84b8e1bc34c9645159e43e1c9a
ogatalars <ogataogara@gmail.com>
2023-07-25 18:19:21 -0300

    Add: New example

---
+++ cap5/ex5_9.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")();
+const produto = prompt("Digite o nome do produto: ");
+
+const num = Number(prompt("Digite a quantidade de produtos: "));
+
+for (let i = 0; i <= num / 2; i++) {
+  console.log(`${produto.padEnd(30)} ${produto.padEnd(30)}`);
+}
+if (num % 2 == 1) {
+  console.log(`${produto.padEnd(30)}`);
+}
